13591ecf4acd01f2b23334210ff548d9bfc3c412
Bartosz Bogdanowicz <wurir1994@gmail.com>
2026-04-10 15:59:15 +0200

    add dataset to excursion, prep for updateBasket

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-"2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
-
-console.log(txt.split(/[\r\n]+/gm));
-
 document.addEventListener('DOMContentLoaded', init)
+
+const basket = []
 
 function init() {
     const uploaderInputEl = document.querySelector('.uploader__input')
+    const excursionsList = document.querySelector('.excursions')
 
     uploaderInputEl.addEventListener('change', uploadFiles)
+    excursionsList.addEventListener('submit', updateBasket)
 }
 
 function uploadFiles(e) {
@@ -74,7 +73,17 @@ function createUserExcursion(excursionObj){
     const childPrice = newExcursionLi.querySelector('.excursions__price-child')
     childPrice.innerText = excursionObj.childPrice
 
+    const adultPriceContainer = newExcursionLi.querySelector('.excursions__field--adult')
+    adultPriceContainer.dataset.priceAdult = excursionObj.adultPrice
+
+    const childPriceContainer = newExcursionLi.querySelector('.excursions__field--child')
+    childPriceContainer.dataset.priceChild = excursionObj.childPrice
+
     excursionsListElement.appendChild(newExcursionLi)
 }
 
-//dodac obsluge form wewnatrz .excursions__form, przypiac nasluchiwanie do ul .excursions, wykorzystac propagacje
+function updateBasket(e){
+    e.preventDefault()
+
+    const excursion = e.target
+}
